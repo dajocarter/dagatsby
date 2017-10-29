0a151cce34dd9d1fbfab45fd2af2d1063762f99d
David Carter <dajocarter@gmail.com>
2017-10-29 11:41:31 -0400

    Editor changes

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,23 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import profilePic from './profile-pic.jpg'
+import profilePic from "./profile-pic.jpg";
 
 class Bio extends React.Component {
   render() {
     return (
       <p>
-        <img
-          src={profilePic}
-          alt={`Kyle Mathews`}
-        />
+        <img src={profilePic} alt={`Kyle Mathews`} />
         Written by <strong>Kyle Mathews</strong> who lives and works in San
-        Francisco building useful things.{' '}
+        Francisco building useful things.{" "}
         <a href="https://twitter.com/kylemathews">
           You should follow him on Twitter
         </a>
       </p>
-    )
+    );
   }
 }
 
-export default Bio
+export default Bio;
