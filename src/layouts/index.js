66cf249aaa3bad36ea8454acbbaa0a857490e44c
David Carter <dajocarter@gmail.com>
2017-10-30 23:11:30 -0400

    Add menu to header
    Create query and styled-components for menu nav

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,22 +9,43 @@ const Header = styled.header`
   top: 0;
   left: 0;
   right: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: #04052e;
   padding: 2rem;
-  &:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
 `;
 
 const LogoHeader = styled.h1`
-  float: left;
+  flex: 0 0 auto;
   margin: 0;
 `;
 
 const LogoLink = StyledLink.extend`
   font-weight: 300;
+`;
+
+const Navigation = styled.nav`
+  flex: 0 0 auto;
+`;
+
+const Menu = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MenuItem = styled.li`
+  flex: 0 0 auto;
+`;
+
+const MenuLink = StyledLink.extend`
+  display: block;
+  text-transform: capitalize;
+  padding: 0.5rem 1rem;
 `;
 
 const Main = styled.main`
@@ -33,12 +54,21 @@ const Main = styled.main`
   min-height: calc(100vh - 164px);
 `;
 
-const Template = ({ location, children }) => (
+const Template = ({ data, children }) => (
   <div>
     <Header>
       <LogoHeader>
-        <LogoLink to="/">David Carter</LogoLink>
+        <LogoLink to={`/`}>David Carter</LogoLink>
       </LogoHeader>
+      <Navigation>
+        <Menu>
+          {data.allSitePage.edges.map(({ node }) => (
+            <MenuItem key={node.path}>
+              <MenuLink to={node.path}>{node.jsonName.slice(0, -5)}</MenuLink>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Navigation>
     </Header>
     <Main>{children()}</Main>
   </div>
@@ -46,8 +76,21 @@ const Template = ({ location, children }) => (
 
 Template.propTypes = {
   children: PropTypes.func,
-  location: PropTypes.object,
+  data: PropTypes.object,
   route: PropTypes.object
 };
 
 export default Template;
+
+export const templateQuery = graphql`
+  query menuQuery {
+    allSitePage(filter: { path: { glob: "/*/", ne: "/dev-404-page/" } }) {
+      edges {
+        node {
+          path
+          jsonName
+        }
+      }
+    }
+  }
+`;
