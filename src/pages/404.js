c0a3a00d8a0814963b9cfd2936ee89a525dc22d0
David Carter <dajocarter@gmail.com>
2017-11-01 16:26:27 -0400

    List all pages on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
+import Link from "gatsby-link";
 
 const FourOhFour = ({ data }) => (
   <div>
@@ -7,6 +8,14 @@ const FourOhFour = ({ data }) => (
     <h1>404 Error</h1>
     <h2>You look lost.</h2>
     <h3>Use the sitemap below to find your way.</h3>
+    <h4>All Pages:</h4>
+    <ul>
+      {data.allSitePage.edges.map(({ node }) => (
+        <li key={node.path}>
+          <Link to={node.path}>{node.path}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
