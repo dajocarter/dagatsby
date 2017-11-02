8dc3a464d162ff342a266520532d215b4f374b2c
David Carter <dajocarter@gmail.com>
2017-11-02 18:12:07 -0400

    Change headline on home page to white

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
+import styled from "styled-components";
+
+const Headline = styled.h1`
+  color: #fff;
+`;
 
 const Index = ({ data }) => (
   <div>
     <Helmet title={data.site.siteMetadata.title} />
-    <h1>Hi, I'm David Carter.</h1>
+    <Headline>Hi, I'm David Carter.</Headline>
     <h3>
       I'm passionate about building responsive websites for small businesses.
     </h3>
