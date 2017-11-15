2178014ef3ea63767e8fd88466eb0dc60082f4ef
David Carter <dajocarter@gmail.com>
2017-11-14 23:20:43 -0500

    Change fonts for typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,19 @@
 import Typography from "typography";
 import fairyGatesTheme from "typography-theme-fairy-gates";
-
+fairyGatesTheme.headerFontFamily = [
+  "Roboto",
+  "Helvetica Neue",
+  "Helvetica",
+  "Arial",
+  "sans-serif"
+];
+fairyGatesTheme.bodyFontFamily = [
+  "Roboto",
+  "Helvetica Neue",
+  "Helvetica",
+  "Arial",
+  "sans-serif"
+];
 const typography = new Typography(fairyGatesTheme);
 
 export default typography;
