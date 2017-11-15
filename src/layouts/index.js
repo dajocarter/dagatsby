b4971dd1b14ca400182699217ae4f000a55e3ff7
David Carter <dajocarter@gmail.com>
2017-11-15 14:12:24 -0500

    Import PrismJS theme
    Used twilight theme

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Header from "../components/Header";
 import "./index.scss";
+require("prismjs/themes/prism-twilight.css");
 
 const Template = ({ children, data, location }) => (
   <div>
