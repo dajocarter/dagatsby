f18927e04fcd28cafe861b5665227caee8d581c0
David Carter <dajocarter@gmail.com>
2017-11-15 00:01:44 -0500

    Forgot to import Header component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Header from "../components/Header";
 import "./index.scss";
 
 const Template = ({ children, data, location }) => (
