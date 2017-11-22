033aa526aa2cba237b299f3f1ddb9fad09bde979
David Carter <dajocarter@gmail.com>
2017-11-22 18:55:37 -0500

    Convert index layout to styled-components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,15 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import Header from "../components/Header";
 import "./index.scss";
+import styled from "styled-components";
 require("prismjs/themes/prism-twilight.css");
 
+const Body = styled.div`
+  background: #252830;
+  color: #cfd2da;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: ${props => (props.home ? `center` : `flex-start`)};
+  align-items: ${props => (props.home ? `center` : `flex-start`)};
+  margin-top: ${props => (props.home ? `0px` : `91px`)};
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 768px;
+  min-height: ${props => (props.home ? `100vh` : `calc(100vh - 91px)`)};
+  padding: 1rem;
+  @media (max-width: 589px) {
+    margin-top: ${props => (props.home ? `0px` : `135px`)};
+    min-height: ${props => (props.home ? `100vh` : `calc(100vh - 135px)`)};
+  }
+`;
+
 const Template = ({ children, data, location }) => (
-  <div>
+  <Body>
     <Header menuItems={data.allSitePage.edges} />
-    <main className={location.pathname === "/" ? `home` : `page`}>
-      {children()}
-    </main>
-  </div>
+    <Main home={location.pathname === "/" ? true : false}>{children()}</Main>
+  </Body>
 );
 
 Template.propTypes = {
