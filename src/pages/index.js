8cbb905654346a1050c2efe2c9d5bf79781a036f
David Carter <dajocarter@gmail.com>
2018-02-11 21:18:59 -0500

    Try prefixing meta image with site url

--- src/pages/index.js
+++ src/pages/index.js
@@ -99,18 +99,18 @@ const Index = ({ data }) => (
       { /* General Tags */}
       <title>{data.site.siteMetadata.title}</title>
       <meta name="description" content={data.site.siteMetadata.description} />
-      <meta name="image" content={profileImg} />
+      <meta name="image" content={`${data.site.siteMetadata.url}${profileImg}`} />
       { /* Facebook Tags */}
       <meta property="og:title" content={data.site.siteMetadata.title} />
       <meta property="og:url" content={data.site.siteMetadata.url} />
       <meta property="og:description" content={data.site.siteMetadata.description} />
-      <meta property="og:image" content={profileImg} />
+      <meta property="og:image" content={`${data.site.siteMetadata.url}${profileImg}`} />
       { /* Twitter Tags */}
       <meta name="twitter:title" content={data.site.siteMetadata.title} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@dajocarter" />
       <meta name="twitter:description" content={data.site.siteMetadata.description} />
-      <meta name="twitter:image" content={profileImg} />
+      <meta name="twitter:image" content={`${data.site.siteMetadata.url}${profileImg}`} />
     </Helmet>
     <Headline>Hi, I'm David Carter.</Headline>
     <Blurb>
