715c2cc97c24022ca0b34b7abb2d3562496c2773
David Carter <dajocarter@gmail.com>
2018-02-18 14:10:26 -0500

    Use grid in ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,38 +2,31 @@ import React from "react";
 import styled from "styled-components";
 import Anchor from "./styled/Anchor";
 import Img from "gatsby-image";
+import { Grid, GridCol } from "griz";
 
-const Item = styled.div`
+const Item = styled(Grid)`
   border: 1px solid #c8c8c8;
   border-radius: 4px;
   max-width: 1024px;
   margin-left: auto;
   margin-right: auto;
   padding: 1rem;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
+
+  & + & {
+    margin-top: 0;
+    padding-top: 1rem;
+  }
 
   &:not(:last-child) {
     margin-bottom: 2rem;
   }
 `;
 
-const ItemImg = styled(Img)`
-  flex: 0 0 auto;
-  @media (max-width: 767px) {
-    width: 50%;
-  }
-`;
+const ItemImg = styled(Img)``;
 
 const ItemContent = styled.div`
-  flex: 1 0 auto;
-  width: calc(100% - 350px);
-  padding-left: 1rem;
-  @media (max-width: 767px) {
-    padding-left: 0;
-    width: 50%;
+  @media (min-width: 768px) {
+    padding-left: 1rem;
   }
 `;
 
@@ -56,21 +49,30 @@ const ItemExcerpt = styled.div``;
 
 const ListItem = props => {
   return (
-    <Item>
+    <Item responsiveMd>
       {props.node.frontmatter.image && (
-        <ItemImg
-          resolutions={props.node.frontmatter.image.childImageSharp.resolutions}
-        />
+        <GridCol>
+          <ItemImg
+            resolutions={
+              props.node.frontmatter.image.childImageSharp.resolutions
+            }
+            sizes={props.node.frontmatter.image.childImageSharp.sizes}
+          />
+        </GridCol>
       )}
-      <ItemContent>
-        <ItemTitle>
-          <ItemLink to={`/posts/${props.node.frontmatter.slug}/`}>
-            {props.node.frontmatter.title}
-          </ItemLink>
-        </ItemTitle>
-        <ItemDate>{props.node.frontmatter.date}</ItemDate>
-        <ItemExcerpt dangerouslySetInnerHTML={{ __html: props.node.excerpt }} />
-      </ItemContent>
+      <GridCol>
+        <ItemContent>
+          <ItemTitle>
+            <ItemLink to={`/posts/${props.node.frontmatter.slug}/`}>
+              {props.node.frontmatter.title}
+            </ItemLink>
+          </ItemTitle>
+          <ItemDate>{props.node.frontmatter.date}</ItemDate>
+          <ItemExcerpt
+            dangerouslySetInnerHTML={{ __html: props.node.excerpt }}
+          />
+        </ItemContent>
+      </GridCol>
     </Item>
   );
 };
