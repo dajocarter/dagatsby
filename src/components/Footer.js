bdd3cfd9951f9c52bc89ff8390a8be43f69c0ccf
David Carter <dajocarter@gmail.com>
2018-02-19 22:32:13 -0500

    Refactor Footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,6 +24,7 @@ const FooterContainer = styled.footer`
 const Copyright = styled.p`
   flex: 0 0 auto;
   margin: 0;
+
   @media (max-width: 488px) {
     margin-top: 1rem;
     order: 2;
@@ -39,29 +40,33 @@ const IconGroup = styled.div`
   align-items: center;
 `;
 
-const IconLink = styled.span`
+const IconWrapper = styled.span`
   background-color: ${props => props.backgroundColor};
   width: 2rem;
   height: 2rem;
   transition: all 0.2s ease-in-out;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+
   &:hover {
     transform: translateY(4px);
     transform-origin: center;
   }
-  &,
-  a {
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  a {
+`;
+
+const IconLink = styled(Anchor)`
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${props => props.color};
+  height: 100%;
+  width: 100%;
+
+  &:hover {
     color: ${props => props.color};
-    height: 100%;
-    width: 100%;
-    &:hover {
-      color: ${props => props.color};
-    }
   }
 `;
 
@@ -70,51 +75,56 @@ const Footer = () => {
     <FooterContainer>
       <Copyright>&copy; 2017 David Carter</Copyright>
       <IconGroup>
-        <IconLink backgroundColor={`#000`} color={`#fff`}>
-          <Anchor
+        <IconWrapper backgroundColor={`#000`}>
+          <IconLink
+            color={`#fff`}
             title={`GitHub @dajocarter`}
             target="_blank"
             href={`//github.com/dajocarter`}
           >
             <GithubIcon />
-          </Anchor>
-        </IconLink>
-        <IconLink backgroundColor={`#0077b5`} color={`#fff`}>
-          <Anchor
+          </IconLink>
+        </IconWrapper>
+        <IconWrapper backgroundColor={`#0077b5`}>
+          <IconLink
+            color={`#fff`}
             title={`LinkedIn @dajocarter`}
             target="_blank"
             href={`//www.linkedin.com/in/dajocarter`}
           >
             <LinkedinIcon />
-          </Anchor>
-        </IconLink>
-        <IconLink backgroundColor={`#f00`} color={`#fff`}>
-          <Anchor
+          </IconLink>
+        </IconWrapper>
+        <IconWrapper backgroundColor={`#f00`}>
+          <IconLink
+            color={`#fff`}
             title={`Gmail @dajocarter`}
             target="_blank"
             href={`mailto:dajocarter@gmail.com`}
           >
             <MailIcon />
-          </Anchor>
-        </IconLink>
-        <IconLink backgroundColor={`#1da1f2`} color={`#fff`}>
-          <Anchor
+          </IconLink>
+        </IconWrapper>
+        <IconWrapper backgroundColor={`#1da1f2`}>
+          <IconLink
+            color={`#fff`}
             title={`Twitter @dajocarter`}
             target="_blank"
             href={`//twitter.com/dajocarter`}
           >
             <TwitterIcon />
-          </Anchor>
-        </IconLink>
-        <IconLink backgroundColor={`#fff`} color={`#000`}>
-          <Anchor
+          </IconLink>
+        </IconWrapper>
+        <IconWrapper backgroundColor={`#fff`}>
+          <IconLink
+            color={`#000`}
             title={`Instagram @dajocarter`}
             target="_blank"
             href={`//www.instagram.com/dajocarter`}
           >
             <InstaIcon />
-          </Anchor>
-        </IconLink>
+          </IconLink>
+        </IconWrapper>
       </IconGroup>
     </FooterContainer>
   );
