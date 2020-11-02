b699b1a0d2d382918490f6e414c7db3fa6ae0db7
David Carter <dajocarter@gmail.com>
2020-11-01 22:47:53 -0800

    change gAnalytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,114 +1,114 @@
 module.exports = {
   siteMetadata: {
-    title: "David Carter | Software Engineer",
-    author: "David Carter",
+    title: 'David Carter | Software Engineer',
+    author: 'David Carter',
     description:
       "Hi, my name's David Carter and I'm a software engineer specializing in frontend development. I'm passionate about learning and motivated to solve problems efficiently.",
-    url: "https://www.dajocarter.com",
+    url: 'https://www.dajocarter.com'
   },
   plugins: [
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "posts",
-        path: `${__dirname}/src/posts`,
-      },
+        name: 'posts',
+        path: `${__dirname}/src/posts`
+      }
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "projects",
-        path: `${__dirname}/src/projects`,
-      },
+        name: 'projects',
+        path: `${__dirname}/src/projects`
+      }
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: `${__dirname}/src/pages`,
-      },
+        name: 'pages',
+        path: `${__dirname}/src/pages`
+      }
     },
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 728,
-            },
+              maxWidth: 728
+            }
           },
           {
-            resolve: "gatsby-remark-responsive-iframe",
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: "margin-bottom: 1.0725rem",
-            },
+              wrapperStyle: 'margin-bottom: 1.0725rem'
+            }
           },
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants",
-        ],
-      },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants'
+        ]
+      }
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-67280088-1",
-      },
+        trackingId: 'UA-108081304-1'
+      }
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "dajocarter",
-        short_name: "dajocarter",
-        start_url: "/",
-        background_color: "#252830",
-        theme_color: "#cfd2da",
-        display: "minimal-ui",
+        name: 'dajocarter',
+        short_name: 'dajocarter',
+        start_url: '/',
+        background_color: '#252830',
+        theme_color: '#cfd2da',
+        display: 'minimal-ui',
         icons: [
           {
-            src: "/favicons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: '/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src: "/favicons/android-chrome-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
+            src: '/favicons/android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
           },
           {
-            src: "/favicons/aaple-touch-icon.png",
-            sizes: "180x180",
-            type: "image/png",
+            src: '/favicons/aaple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
           },
           {
-            src: "/favicons/favicon-16x16.png",
-            sizes: "16x16",
-            type: "image/png",
+            src: '/favicons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
           },
           {
-            src: "/favicons/favicon-32x32.png",
-            sizes: "32x32",
-            type: "image/png",
+            src: '/favicons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
           },
           {
-            src: "/favicons/mstile-150x150.png",
-            sizes: "150x150",
-            type: "image/png",
-          },
-        ],
-      },
+            src: '/favicons/mstile-150x150.png',
+            sizes: '150x150',
+            type: 'image/png'
+          }
+        ]
+      }
     },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-typography",
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: "src/utils/typography.js",
-      },
+        pathToConfigModule: 'src/utils/typography.js'
+      }
     },
-    "gatsby-plugin-styled-components",
-  ],
-};
+    'gatsby-plugin-styled-components'
+  ]
+}
